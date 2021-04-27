2bfee3d195b75a7f440baeb50cf7b66e1f5683bb
ex_010467500 <xuyunjin@odianyun.com.cn>
2021-04-27 16:54:53 +0800

    动态入口和动态出口

--- vue.config.js
+++ vue.config.js
@@ -3,30 +3,58 @@ const path = require('path')
 
 
 
-// const pages = function(){
-//   let jsPath = path.resolve(__dirname,'src/pages')
-//   let jsFiles = glob.sync(jsPath+'/**/index.js')
-//   let map = {}
-//   jsFiles.forEach(file=>{
-//     let fileDirName = file.match(/\/([^/]+)\/index.js/i)[1]
-//     // console.log(fileDirName)
-//     map[fileDirName] = {
-//       entry:`src/pages/${fileDirName}`,
-//       template:'public/index.html',
-//       title:`${fileDirName}`,
-//       filename:`${fileDirName}/${fileDirName}.html`,
-//       chunks:['vendors','common',`${fileDirName}`]
-//     }
-//   })
-//   return map
-// }
+const pages = function(){
+  let jsPath = path.resolve(__dirname,'src/pages')
+  let jsFiles = glob.sync(jsPath+'/**/index.js')
+  let entryMap = {}
+  let outputMap = {}
+  jsFiles.forEach(file=>{
+    // let fileDirName = file.match(/\/([^/]+)\/index.js/i)[1]
+    // console.log(file)
+    let fullPath = file.split('pages/')[1]
+    console.log(fullPath)
+    let fileDir = fullPath.split('/index.js')[0]
+    let fName = fullPath.match(/([^/]+)\/index.js/i)[1]
+    console.log(fileDir)
+    console.log(fName)
+    entryMap[fName] = {
+      entry:`src/pages/${fileDir}`,
+      template:'public/index.html',
+      title:`${fName}`,
+      filename:`${fileDir}.html`,
+      chunks:['vendors','common',`${fName}`]
+    }
+    outputMap[fName] = fileDir
+    // {
+    //   index: {
+    //     entry: 'src/pages/index',
+    //     template: 'public/index.html',
+    //     title: 'index',
+    //     filename: 'index.html',
+    //     chunks: [ 'vendors', 'index' ]
+    //   },
+    //   'sub-form': {
+    //     entry: 'src/pages/subscribe/sub-form',
+    //     template: 'public/index.html',
+    //     title: 'sub-form',
+    //     filename: 'subscribe/sub-form.html',
+    //     chunks: [ 'vendors', 'common', 'sub-form' ]
+    //   },
+    //   'sub-list': {
+    //     entry: 'src/pages/subscribe/sub-list',
+    //     template: 'public/index.html',
+    //     title: 'sub-list',
+    //     filename: 'subscribe/sub-list.html',
+    //     chunks: [ 'vendors', 'common', 'sub-list' ]
+    //   }
+    // }
+  })
+  return {entryMap, outputMap}
+}
 // console.log(pages())
 
-const outputMap = {
-  index:'index',
-  'sub-form':'subscribe/sub-form',
-  'sub-list':'subscribe/sub-list'
-}
+const {entryMap, outputMap} = pages()
+
 
 module.exports = {
   outputDir:__dirname+'/dist',
@@ -61,27 +89,5 @@ module.exports = {
       }
     }
   },
-  pages:{
-    index: {
-      entry: 'src/pages/index',
-      template: 'public/index.html',
-      title: 'index',
-      filename: 'index.html',
-      chunks: [ 'vendors', 'index' ]
-    },
-    'sub-form': {
-      entry: 'src/pages/subscribe/sub-form',
-      template: 'public/index.html',
-      title: 'sub-form',
-      filename: 'subscribe/sub-form.html',
-      chunks: [ 'vendors', 'common', 'sub-form' ]
-    },
-    'sub-list': {
-      entry: 'src/pages/subscribe/sub-list',
-      template: 'public/index.html',
-      title: 'sub-list',
-      filename: 'subscribe/sub-list.html',
-      chunks: [ 'vendors', 'common', 'sub-list' ]
-    }
-  }
+  pages:entryMap
 }
